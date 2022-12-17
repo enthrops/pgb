67af7b7346e05a4c9b442943d8480f7a10e6c11b
Dmitry Gubitskiy <d.gubitskiy@gmail.com>
2022-12-18 02:20:43 +0300

    Executable

--- lib/pgb/executable.rb
+++ lib/pgb/executable.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 module PGB
+  # TODO Support lazy via cursors
   module Executable
-    include Enumerable
+    def to_a
+      PGB.execute(self)
+    end
 
-    def each
-      PGB.execute(self).each { yield _1 }
+    def run
+      to_a
+    end
+
+    def method_missing(method, *args, &block)
+      if Array.public_method_defined?(method)
+        to_a.public_send(method, *args, &block)
+      else
+        super
+      end
     end
   end
 end
