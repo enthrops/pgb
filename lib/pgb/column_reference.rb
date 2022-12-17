2ff100c47bec7f5114d6b6da0254716fc6ed5e4e
Dmitry Gubitskiy <d.gubitskiy@gmail.com>
2022-12-18 01:56:44 +0300

    Column reference

--- lib/pgb/column_reference.rb
+++ lib/pgb/column_reference.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 module PGB
+  # TODO Need separation between Identifier and ColumnReference?
   class ColumnReference < Expression
     def initialize(table, column)
-      @value = "#{to_identifier(table)}.#{to_identifier(column)}"
+      @value = "#{Identifier.new(table)}.#{Identifier.new(column)}"
     end
 
     def to_sql
