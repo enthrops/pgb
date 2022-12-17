6564526d2a86a7db0bb03e169c3c3f1c5ac11d0b
Dmitry Gubitskiy <d.gubitskiy@gmail.com>
2022-12-18 01:15:16 +0300

    Literal

--- lib/pgb/literal.rb
+++ lib/pgb/literal.rb
@@ -3,27 +3,28 @@
 module PGB
   class Literal < Expression
     def initialize(value)
-      @value = to_db(value).to_s
+      @value = to_db(value)
     end
 
     def to_sql
       value
     end
 
-    private
+    protected
 
     attr_reader :value
 
-    # TODO More types
+    private
+
+    # TODO Correctly represent different numeric types
     def to_db(value)
       case value
+      when Literal
+        value.value
       when String, Symbol
         "'#{value.to_s.gsub("'", "''")}'"
-      when Numeric
-        # TODO Correctly convert different numeric types
-        value
       else
-        raise Error, "Unsupported type for literal: #{value.class}"
+        value.to_s
       end
     end
   end
